f15a466bd621c35af4529fd1128bcdfcb310d5cb
SangitThakuri <162006772+SangitThakuri@users.noreply.github.com>
2026-06-30 23:17:08 +0900

    Modify base path for GitHub Actions
    Update base path configuration based on build environment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/devbits.tools/",
+  // If the build environment is GitHub Actions, use the subfolder path. Otherwise, use the root path.
+  base: process.env.GITHUB_ACTIONS ? "/devbits.tools/" : "/",
   plugins: [react(), tailwindcss()],
 })
