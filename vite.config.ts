a618f168314e28db166146b4b6946f9bb4f4bd58
SangitThakuri <162006772+SangitThakuri@users.noreply.github.com>
2026-06-30 23:23:33 +0900

    Remove comment about build environment in config
    Removed comment regarding GitHub Actions build environment.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  // If the build environment is GitHub Actions, use the subfolder path. Otherwise, use the root path.
   base: process.env.GITHUB_ACTIONS ? "/devbits.tools/" : "/",
   plugins: [react(), tailwindcss()],
 })
